55b310e70b06ea5ac539e8ba15784e4394d39053
--global <--global>
2025-04-01 16:21:02 +0400

    first commit

--- src/app/modules/layout/layout-routing.module.ts
+++ src/app/modules/layout/layout-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {Dashboard} from '../../pages/dashboard/dashboard';
+import {Dashboard} from '@pages/dashboard/dashboard';
 
 const routes: Routes = [
   {
